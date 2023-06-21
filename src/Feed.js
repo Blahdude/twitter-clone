3dae77c602c9cc58ec936f1e4086af4e5192bce0
Oliver Camp <ocamp@umich.edu>
2023-06-21 12:31:17 -0700

    first commit

---
+++ src/Feed.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Feed.css'
+
+function Feed() {
+  return (
+    <div>
+        {/* Header */}
+        <h2>This is the home</h2>
+
+        {/* TweetBox */}
+
+        {/* post */}
+        {/* post */}
+        {/* post */}
+        {/* post */}
+        {/* post */}
+        {/* post */}
+    </div>
+  )
+}
+
+export default Feed
